b36db0a311a7c309eaca02c90794f33a9f7b3703
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-30 18:08:45 -0400

    Update MolochSummoner.sol
    update to latest lexdao

--- contracts/MolochSummoner.sol
+++ contracts/MolochSummoner.sol
@@ -5,38 +5,37 @@ import "./ISummonMinion.sol";
 
 contract MolochSummoner {
     Moloch private baal;
-    ISummonMinion public minionSummoner;
-    address[] public molochs;
 
-    event Summoned(address indexed mol, address[] indexed _summoners);
-    
-    constructor(address _minionSummoner) public {
-       minionSummoner = ISummonMinion(_minionSummoner);
-    }
-
+    event SummonMoloch(address indexed baal, address depositToken, address wrapperToken, address[] indexed summoner, uint256[] indexed summonerShares, uint256 summoningDeposit, uint256 proposalDeposit, uint256 processingReward, uint256 periodDuration, uint256 votingPeriodLength, uint256 gracePeriodLength, uint256 dilutionBound, uint256 summoningTime);
+ 
     function summonMoloch(
-        address[] memory _summoners,
-        address[] memory _approvedTokens,
+        address _depositToken,
+        address _wrapperToken,
+        address[] memory _summoner,
+        uint256[] memory _summonerShares,
+        uint256 _summonerDeposit,
+        uint256 _proposalDeposit,
+        uint256 _processingReward,
         uint256 _periodDuration,
         uint256 _votingPeriodLength,
         uint256 _gracePeriodLength,
-        uint256 _proposalDeposit,
-        uint256 _dilutionBound,
-        uint256 _processingReward) public {
-
+        uint256 _dilutionBound
+    ) public {
         baal = new Moloch(
-            _summoners,
-            _approvedTokens,
+            _depositToken,
+            _wrapperToken,
+            _summoner,
+            _summonerShares,
+            _summonerDeposit,
+            _proposalDeposit,
+            _processingReward,
             _periodDuration,
             _votingPeriodLength,
             _gracePeriodLength,
-            _proposalDeposit,
-            _dilutionBound,
-            _processingReward);
-        
-        address mol = address(baal);
-        molochs.push(mol);
-        minionSummoner.summonMinion(mol, _approvedTokens[0]);
-        emit Summoned(mol, _summoners);
+            _dilutionBound);
+
+        IERC20(_depositToken).transferFrom(msg.sender, address(baal), _summonerDeposit); // transfer summoner deposit to new moloch
+
+        emit SummonMoloch(address(baal), _depositToken, _wrapperToken, _summoner, _summonerShares, _summonerDeposit, _proposalDeposit, _processingReward, _periodDuration, _votingPeriodLength, _gracePeriodLength, _dilutionBound, now);
     }
 }
